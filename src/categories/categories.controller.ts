a241214f0840fdfbda2470ad6268c8206493c6da
Murat Aznabakiyev <m.aznabakiyev@petrel.ai>
2024-01-25 12:22:37 +0600

    feat: categories service

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
+import { CategoriesService } from "./categories.service";
+import { CreateCategoryDto } from "./dto/create-category.dto";
 
-@Controller('categories')
-export class CategoriesController {}
+@Controller("categories")
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  create(@Body() createCategoryDto: any) {
+    return this.categoriesService.create(createCategoryDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.categoriesService.findOne(id);
+  }
+
+  @Patch(":id")
+  update(
+    @Param("id") id: string,
+    @Body() updateCategoryDto: CreateCategoryDto
+  ) {
+    return this.categoriesService.update(id, updateCategoryDto);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.categoriesService.remove(id);
+  }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Categories } from "./categories.model";
+import { CreateCategoryDto } from "./dto/create-category.dto";
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectModel(Categories)
+    private categoryModel: typeof Categories
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return this.categoryModel.create(createCategoryDto);
+  }
+
+  async findAll(): Promise<Categories[]> {
+    return this.categoryModel.findAll();
+  }
+
+  async findOne(id: string): Promise<Categories> {
+    return this.categoryModel.findByPk(id);
+  }
+
+  async update(
+    id: string,
+    updateCategoryDto: CreateCategoryDto
+  ): Promise<void> {
+    await this.categoryModel.update(updateCategoryDto, {
+      where: { id },
+    });
+  }
+
+  async remove(id: string): Promise<void> {
+    const category = await this.findOne(id);
+    await category.destroy();
+  }
+}
